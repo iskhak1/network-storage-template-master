55a338f4bfb88708fe1ab78cc5c68e2a4d1e3a7f
Iskhak <isakasi12242001@gmail.com>
2022-05-06 07:09:19 +0300

    done homework2

---
+++ server/NettyEchoServerHandler.java
@@ -0,0 +1,21 @@
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+public class NettyEchoServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Server received: " + msg);
+        ctx.write("hello from server " + msg + "\r\n");
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
